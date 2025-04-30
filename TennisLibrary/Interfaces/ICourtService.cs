915065f738868d99819fafa092e6727a578633f0
ICOnce <noaellegaard@gmail.com>
2025-04-30 11:09:07 +0200

    Added base for court

---
+++ TennisLibrary/Interfaces/ICourtService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TennisLibrary.Models;
+
+namespace TennisLibrary.Interfaces
+{
+    public interface ICourtService
+    {
+        Task<bool> CreateCourtAsync(Court court);
+        Task<Court> GetCourtAsync(string name);
+        Task<List<Court>> GetCourtsOfTypeAsync(string name);
+        Task<bool> UpdateCourtAsync(string name, Court newCourt);
+        Task<bool> DeleteCourtAsync(string name);
+    }
+}
