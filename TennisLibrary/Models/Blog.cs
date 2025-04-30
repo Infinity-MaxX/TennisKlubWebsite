7f2fa72264100c731867686466f0c32e8db39901
Infinity-MaxX <katerina@oestergaard.com>
2025-04-30 12:58:23 +0200

    Finished IBlogService and Blog. BlogService template for methods up.

--- TennisLibrary/Models/Blog.cs
+++ TennisLibrary/Models/Blog.cs
@@ -9,12 +9,12 @@ namespace TennisLibrary.Models
     public class Blog
     {
         #region Instances
-        private int _blogpostid;
+        private int _blogPostId;
         private DateTime _date;
         #endregion
 
         #region Properties
-        public int ID { get { return _blogpostid; } }
+        public int ID { get { return _blogPostId; } }
         public string Author { get; set; }
         public string? Title { get; set; }
         public string Body { get; set; }
@@ -24,15 +24,19 @@ namespace TennisLibrary.Models
         #region Constructor
         public Blog(string author, string? title, string body)
         {
-            _blogpostid++;
+            //_blogPostId++;
             Author = author;
             Title = title;
             Body = body;
+            _date = DateTime.Now;
         }
         #endregion
 
         #region Methods
-
+        public override string ToString()
+        {
+            return $"Author: {Author}, Title: {Title}\nBody: {Body}\nDate: {Date}";
+        }
         #endregion
     }
 }
--- TennisLibrary/Services/BlogService.cs
+++ TennisLibrary/Services/BlogService.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TennisLibrary.Interfaces;
+using TennisLibrary.Models;
 
 namespace TennisLibrary.Services
 {
-    public class BlogService
+    public class BlogService : IBlogService
     {
         #region Instances
 
@@ -21,7 +23,34 @@ namespace TennisLibrary.Services
         #endregion
 
         #region Methods
+        public Task<bool> CreatePostAsync(Blog newPost)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<bool> DeletePostAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Blog>> GetAllPostsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Blog> GetByAuthorAsync(string author)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Blog> GetByDateAsync(DateOnly date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdatePostAsync(string post, Blog newPost)
+        {
+            throw new NotImplementedException();
+        }
         #endregion
     }
 }
