314c949ef96fa2b4807227239cc6958ad4d339c2
ICOnce <noaellegaard@gmail.com>
2025-05-02 09:48:22 +0200

    Moved everything from class library to razor page project

---
+++ TennisWebsite/ClassLibrary/ConnectionManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisLibrary
+{
+    public class ConnectionManager
+    {
+        public static string ConnectionString { get { return _connectionString; }}
+        private static string _connectionString = @"Data Source=mssql13.unoeuro.com;Initial Catalog=troglodyte_dk_db_troglodytedb;User ID=troglodyte_dk;Password=9Datmy4x5RrekfGng6dA;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    }
+}
