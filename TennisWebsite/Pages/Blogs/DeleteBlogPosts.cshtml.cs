1e97dc1dbed339573c745d4fecfa5106a65b255b
Infinity-MaxX <katerina@oestergaard.com>
2025-05-02 12:28:26 +0200

    Created templates for all BlogPosts.

---
+++ TennisWebsite/Pages/Blogs/DeleteBlogPosts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TennisWebsite.Pages.Blogs
+{
+    public class DeleteBlogPostsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
