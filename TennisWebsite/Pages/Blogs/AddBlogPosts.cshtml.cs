4ea4b8c36c1381d1b9b149441b8c2f5a9a1ac701
Infinity-MaxX <katerina@oestergaard.com>
2025-05-05 18:08:19 +0200

    Basic functionality of AddBlogPost.

--- TennisWebsite/Pages/Blogs/AddBlogPosts.cshtml.cs
+++ TennisWebsite/Pages/Blogs/AddBlogPosts.cshtml.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Hosting;
+using TennisLibrary.Helpers;
 using TennisLibrary.Interfaces;
 using TennisLibrary.Models;
+using TennisLibrary.Services;
 
 namespace TennisWebsite.Pages.Blogs
 {
@@ -15,19 +17,39 @@ namespace TennisWebsite.Pages.Blogs
         #region Properties
         [BindProperty]
         public Blog BlogPost { get; set; }
+
+        [BindProperty]
+        public int ID { get; private set; }
+
+        [BindProperty]
+        public string Author { get; set; }
+
+        [BindProperty]
+        public string? Title { get; set; }
+
+        [BindProperty]
+        public string Body { get; set; }
+
+        [BindProperty]
+        public DateTime Date {  get; set; }
+
+        [BindProperty]
+        public AccessLevel AccessLevel { get; set; }
+
+        public int? SessionAccessLevel { get; private set; }
         #endregion
 
         #region Constructor
-        public AddBlogPostsModel(IBlogService blogPostService)
+        public AddBlogPostsModel()
         {
-            _blogPostService = blogPostService;
+            _blogPostService = new BlogService();
         }
         #endregion
 
         #region Methods
         public void OnGet()
         {
-
+            SessionAccessLevel = HttpContext.Session.GetInt32("AccessLevel");
         }
         public async Task<IActionResult> OnPostAsync()
         {
