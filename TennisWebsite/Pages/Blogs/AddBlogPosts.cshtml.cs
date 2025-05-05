57e3c4dfb4b67a1909ab270cf78600ce3a834d9c
Infinity-MaxX <katerina@oestergaard.com>
2025-05-05 14:30:36 +0200

    Midway point for RazorPages for Blog.

--- TennisWebsite/Pages/Blogs/AddBlogPosts.cshtml.cs
+++ TennisWebsite/Pages/Blogs/AddBlogPosts.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Hosting;
 using TennisLibrary.Interfaces;
 using TennisLibrary.Models;
 
@@ -7,8 +8,32 @@ namespace TennisWebsite.Pages.Blogs
 {
     public class AddBlogPostsModel : PageModel
     {
+        #region Instances
+        private IBlogService _blogPostService;
+        #endregion
+
+        #region Properties
+        [BindProperty]
+        public Blog BlogPost { get; set; }
+        #endregion
+
+        #region Constructor
+        public AddBlogPostsModel(IBlogService blogPostService)
+        {
+            _blogPostService = blogPostService;
+        }
+        #endregion
+
+        #region Methods
         public void OnGet()
         {
+
         }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await _blogPostService.CreatePostAsync(new Blog(BlogPost.Author, BlogPost.Title, BlogPost.Body));
+            return RedirectToPage("ShowPosts");
+        }
+        #endregion
     }
 }
--- TennisWebsite/Pages/Blogs/ShowBlogPosts.cshtml.cs
+++ TennisWebsite/Pages/Blogs/ShowBlogPosts.cshtml.cs
@@ -1,14 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Hosting;
 using TennisLibrary.Interfaces;
 using TennisLibrary.Models;
+using TennisLibrary.Services;
 
 namespace TennisWebsite.Pages.Blogs
 {
     public class ShowBlogPostsModel : PageModel
     {
-        public void OnGet()
+        #region Instances
+        private IBlogService _blogPostService;
+        #endregion
+
+        #region Properties
+        public List<Blog> BlogPosts { get; set; }
+        #endregion
+
+        #region Constructor
+        public ShowBlogPostsModel(IBlogService blogRepo)
         {
+            _blogPostService = new BlogService();
         }
+        #endregion
+
+        #region Methods
+        public async Task OnGetAsync()
+        {
+            BlogPosts = await _blogPostService.GetAllPostsAsync();
+        }
+
+        /// <summary>
+        /// A delete function as one does not need an extra page to delete a hotel.
+        /// </summary>
+        public async Task<IActionResult> OnGetDelete(int DeleteNo)
+        {
+            await _blogPostService.DeletePostAsync(DeleteNo);
+            return RedirectToPage("ShowPosts");
+        }
+        #endregion
     }
 }
