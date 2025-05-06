e75d030a6aced899e21d512118a503826321a705
ICOnce <noaellegaard@gmail.com>
2025-05-06 10:01:59 +0200

    Added add functionality page for Court

---
+++ TennisWebsite/Pages/CourtPages/ShowCourts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TennisWebsite.Pages.CourtPages
+{
+    public class ShowCourtsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
