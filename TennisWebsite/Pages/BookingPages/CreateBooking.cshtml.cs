c20baf841aea61da532f97ab73f13b33f08015ba
Lasse Hemming Kellberg <lak006@edu.zealand.dk>
2025-05-08 17:18:40 +0200

    CreateBooking issues

--- TennisWebsite/Pages/BookingPages/CreateBooking.cshtml.cs
+++ TennisWebsite/Pages/BookingPages/CreateBooking.cshtml.cs
@@ -9,6 +9,13 @@ namespace TennisWebsite.Pages.BookingPages
     {
         [BindProperty]
         public Booking booking { get; set; }
+
+        [BindProperty]
+        public string player1 { get; set; }
+        
+        [BindProperty]
+        public string player2 { get; set; }
+
         private BookingService bs;
         private CourtService cs;
         private UserService us;
@@ -27,6 +34,17 @@ namespace TennisWebsite.Pages.BookingPages
             booking.Court = await cs.GetCourtAsync(court);
             booking.Start = tempTime;
             booking.End = tempTime.AddHours(1);
+        }
+
+        public async Task<IActionResult> OnPostCreateAsync()
+        {
+
+            //if (/*player1 != null && player2 != null && court != null /*&& start >= DateTime.Now && end > start*/ 1 == 1)
+            //{
+            //    return RedirectToPage("/index");
+            //}
+            //return Page();
+            return Page(booking.Start, booking.Court);
         }
     }
 }
