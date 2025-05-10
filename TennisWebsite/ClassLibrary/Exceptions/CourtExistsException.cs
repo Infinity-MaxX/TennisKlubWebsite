4c73e29b199c3ba6b8bd057b432c6d8e4ef83752
ICOnce <noaellegaard@gmail.com>
2025-05-10 19:58:40 +0200

    Decided to not do custom exception classes, just messages

--- TennisWebsite/ClassLibrary/Exceptions/CourtExistsException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TennisWebsite.ClassLibrary.Exceptions
-{
-    public class CourtExistsException: Exception
-    {
-        public CourtExistsException() { }
-        public CourtExistsException(string message) : base(message){ }
-    }
-}
