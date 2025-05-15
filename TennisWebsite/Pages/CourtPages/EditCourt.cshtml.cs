a31e1785e7887b35a24d7dcac7c3c085f964afaa
ICOnce <noaellegaard@gmail.com>
2025-05-15 11:11:49 +0200

    Added modal for deletion of Court

--- TennisWebsite/Pages/CourtPages/EditCourt.cshtml.cs
+++ TennisWebsite/Pages/CourtPages/EditCourt.cshtml.cs
@@ -1,17 +1,52 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TennisLibrary.Models;
+using TennisLibrary.Services;
 
 namespace TennisWebsite.Pages.CourtPages
 {
     public class EditCourtModel : PageModel
     {
-        public async Task<IActionResult> OnGetAsync()
+
+
+        [BindProperty]
+        public Court newCourt { get; set; }
+
+        [BindProperty]
+        public string OldCourt { get; set; }
+
+        private CourtService cs;
+
+        public EditCourtModel()
         {
+            cs = new CourtService();
+            newCourt = new Court();
+        }
+
+        public async Task<IActionResult> OnGetAsync(string courtName)
+        {
+            newCourt = await cs.GetCourtAsync(courtName);
+            newCourt.LastMaintenance = DateOnly.FromDateTime(DateTime.Now);
             if (HttpContext.Session.GetString("Username") == null)
             {
                 return RedirectToPage("/Users/Login");
             }
+            OldCourt = newCourt.Name;
             return Page();
         }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            try
+            {
+                await cs.UpdateCourtAsync(OldCourt, newCourt.Name, newCourt.LastMaintenance);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("NewCourt.Name", ex.Message);
+                return Page();
+            }
+            return RedirectToPage("ShowCourts");
+        }
+
     }
 }
