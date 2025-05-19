a53077c4045f301c0e6b084dc58c3c43acd1833b
ICOnce <noaellegaard@gmail.com>
2025-05-19 08:51:57 +0200

    Beginning work on event service

--- TennisWebsite/ClassLibrary/Services/EventService.cs
+++ TennisWebsite/ClassLibrary/Services/EventService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
+using TennisLibrary;
 using TennisLibrary.Interfaces;
 using TennisLibrary.Models;
 using TennisWebsite.Interfaces;
@@ -15,41 +16,92 @@ namespace TennisWebsite.Services
 {
     public class EventService : IEventService
     {
-        #region Instances
-        List<Event> _events;
-        #endregion
 
-        #region Properties
-        public int EventCount { get { return _events.Count; } }
-
+        #region instance fields
+        private string insertEventSql = "INSERT INTO TENNISEVENT " +
+            "(OrgUsername, StartDate, EndDate, EventType, Name, Description, MinAttendees, MaxAttendees)" +
+            "VALUES (@OrgUsername, @StartDate, @EndDate, @EventType, @Name, @Description, @MinAttendees, @MaxAttendees)";
+        private string deleteSql = "DELETE FROM TENNISEVENT WHERE EventID = @Id";
         #endregion
 
         #region Constructors
+        public EventService()
+        {
+
+        }
 
         #endregion
 
         #region Methods
-        public Task<bool> CreateEventAsync(Event evnt)
+        public async Task<bool> CreateEventAsync(Event newEvent)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionManager.ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+
+                    SqlCommand command = new SqlCommand(insertEventSql, connection);
+                    command.Parameters.AddWithValue("@OrgUsername", newEvent.Organiser);
+                    command.Parameters.AddWithValue("@StartDate", newEvent.Start);
+                    command.Parameters.AddWithValue("@EndDate", newEvent.End);
+                    command.Parameters.AddWithValue("@EventType", newEvent.Type);
+                    command.Parameters.AddWithValue("@Name", newEvent.Name);
+                    command.Parameters.AddWithValue("@Description", newEvent.Description);
+                    command.Parameters.AddWithValue("@MinAttendees", newEvent.MinParticipants);
+                    command.Parameters.AddWithValue("@MaxAttendees", newEvent.MaxParticipants);
+                    int noOfRows = await command.ExecuteNonQueryAsync();
+
+                    return noOfRows == 1;
+                }
+
+                catch (SqlException sqlx)
+                {
+                    Console.WriteLine(sqlx.Number);
+                    Console.WriteLine(sqlx.Message);
+                    return false;
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+            }
+        }
+
+        public async Task<bool> DeleteEventAsync(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionManager.ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+
+                    SqlCommand command = new SqlCommand(deleteSql, connection);
+                    command.Parameters.AddWithValue("@Id", id);
+                    int noOfRows = await command.ExecuteNonQueryAsync();
+
+                    return noOfRows == 1;
+                }
+
+                catch (SqlException sqlx)
+                {
+                    Console.WriteLine(sqlx.Number);
+                    Console.WriteLine(sqlx.Message);
+                    return false;
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+            }
+        }
+
+        public Task<List<Event>> GetAllEventsAsync()
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> DeleteEventAsync(DateTime date)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<Event>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<User>> GetAllParticipantsForEventAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Event> GetEvent(DateTime date)
+        public Task<Event> GetEvent(int id)
         {
             throw new NotImplementedException();
         }
@@ -59,13 +111,27 @@ namespace TennisWebsite.Services
             throw new NotImplementedException();
         }
 
-        public Task<User> GetUserByEvent(User user)
+        public Task<Event> GetEventsByOrganiser(string orgName)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> UpdateEvent(User organiser, string name, string description, 
-            string start, string end, int maxParticipants, int minParticipants)
+        public Task<List<User>> GetParticipantsByEventAsync(int eventID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> JoinEvent(int eventID, string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> LeaveEvent(int eventID, string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateEvent(string organiser, string name, string description, string start, string end, int maxParticipants, int minParticipants)
         {
             throw new NotImplementedException();
         }
