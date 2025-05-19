f8ca55ed0b11367e9ef669160fbce5967535dc4e
Infinity-MaxX <katerina@oestergaard.com>
2025-05-19 13:14:33 +0200

    Created IGearService, Gear, and GearService. Finished IGearService and Gear. Begun GearService.

---
+++ TennisWebsite/ClassLibrary/Interfaces/IGearService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TennisLibrary.Models;
+using TennisWebsite.ClassLibrary.Models;
+namespace TennisWebsite.ClassLibrary.Interfaces;
+public interface IGearService
+{
+    public int Count { get; }
+    public bool Status { get; set; }
+    public void AddGear(string name, string? description);
+    public void BookGear(int bookingID, int gearID);
+    //public void BorrowGear(int id);
+    public void CheckStatus(int id);
+    public void DeleteGear(int id);
+    public List<Gear> GetAll();
+    public Gear GetGear(int id);
+    //public void ReturnGear(int id);
+}
---
+++ TennisWebsite/ClassLibrary/Services/GearService.cs
@@ -0,0 +1,57 @@
+﻿using TennisWebsite.ClassLibrary.Interfaces;
+using TennisWebsite.ClassLibrary.Models;
+
+namespace TennisWebsite.ClassLibrary.Services
+{
+    public class GearService : IGearService
+    {
+        #region Instances
+        private List<Gear> _gearRepo;
+        #endregion
+
+        #region Properties
+        public int Count { get { return _gearRepo.Count(); } }
+        public bool Status { get; set; }
+        #endregion
+
+        #region Constructor
+        public GearService()
+        {
+            _gearRepo = new List<Gear>();
+        }
+        #endregion
+
+        #region Methods
+
+        public void AddGear(string name, string? description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BookGear(int bookingID, int gearID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckStatus(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteGear(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Gear> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Gear GetGear(int id)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
