cf42964abcc125df12480b87b12cf316d28bed8a
ICOnce <noaellegaard@gmail.com>
2025-05-27 08:58:49 +0200

    Added AddEvent

--- TennisWebsite/Pages/Events/AddEvent.cshtml.cs
+++ TennisWebsite/Pages/Events/AddEvent.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TennisWebsite.Interfaces;
+using TennisWebsite.Models;
 
 namespace TennisWebsite.Pages.Events
 {
     public class AddEventModel : PageModel
     {
+
+        [BindProperty]
+        public Event NewEvent { get; set; }
+
+        private IEventService _eventService;
+
+        public AddEventModel(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await _eventService.CreateEventAsync(NewEvent);
+            return RedirectToPage("/Pages/Events/ShowEvents");
         }
     }
 }
